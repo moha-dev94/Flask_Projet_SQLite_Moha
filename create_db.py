bfc9b5933196a8b06e830133fbc73bb214ebe9d2
moha-dev94 <ennadifima@gmail.com>
2026-01-28 17:03:39 +0100

    Modify default accounts and book stock in database
    Updated default accounts and initial book stock.

--- create_db.py
+++ create_db.py
@@ -1,28 +1,27 @@
 import sqlite3
+import os
+
+# On force le travail dans le bon répertoire
+path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(path)
 
 connection = sqlite3.connect('database.db')
-
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUPONT', 'Emilie', '123, Rue des Lilas, 75001 Paris'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('TREMBLAY', 'Antoine', '1010, Boulevard de la Mer, 13008 Marseille'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LAMBERT', 'Sarah', '222, Avenue de la Liberté, 59000 Lille'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGNON', 'Nicolas', '456, Boulevard des Cerisiers, 69003 Lyon'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
-cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
-# Création des comptes par défaut
-cur.execute("INSERT INTO clients (nom, prenom, username, password, role) VALUES (?, ?, ?, ?, ?)",
-            ('Admin', 'System', 'admin', 'password', 'admin'))
-cur.execute("INSERT INTO clients (nom, prenom, username, password, role) VALUES (?, ?, ?, ?, ?)",
-            ('Dupont', 'Jean', 'user', '12345', 'user'))
-#Ajouts des livres
+# Comptes par défaut
+cur.execute("INSERT INTO clients (nom, prenom, username, password, role, adresse) VALUES (?, ?, ?, ?, ?, ?)",
+            ('Admin', 'System', 'admin', 'password', 'admin', 'Local Admin'))
+cur.execute("INSERT INTO clients (nom, prenom, username, password, role, adresse) VALUES (?, ?, ?, ?, ?, ?)",
+            ('Dupont', 'Jean', 'user', '12345', 'user', '123 Rue de Paris'))
+
+# Stock initial de livres
 cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)", ('Le Petit Prince', 'Saint-Exupéry', 5))
 cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)", ('1984', 'George Orwell', 2))
-cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)", ('Le Cid', 'Corneille', 0))
+cur.execute("INSERT INTO livres (titre, auteur, stock) VALUES (?, ?, ?)", ('Le Cid', 'Corneille', 1))
+
 connection.commit()
 connection.close()
+print(f"Base de données créée avec succès dans {path}")
